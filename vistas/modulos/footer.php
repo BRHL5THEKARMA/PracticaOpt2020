e5c0bfab791e0e3948d50a0335a78633aa532a0a
Benjamin <sergiobenjamindx@gmail.com>
2020-04-10 16:03:41 -0600

    derechos de autor 1.0

--- vistas/modulos/footer.php
+++ vistas/modulos/footer.php
@@ -1,6 +1,6 @@
 <footer class="main-footer">
 	
-	<strong>Copyright &copy; 2017 <a href="https://www.tutorialesatualcance.com" target="_blank">Tutoriales a tu Alcance</a>.</strong>
+	<strong>Copyright &copy; 2020 <a href="https://www.xvideos.com" target="_blank">Bryan y Benja estuvieron aqui :v... #NoSalgoDeCasa</a>.</strong>
 
 	Todos los derechos reservados.
 
