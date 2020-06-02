14ecb190724320d823637865793d349c79f96299
BRHL5THEKARMA <39506787+BRHL5THEKARMA@users.noreply.github.com>
2020-06-02 16:17:16 -0600

    Version FInal Stable2.1.

--- vistas/modulos/inicio/productos-recientes.php
+++ vistas/modulos/inicio/productos-recientes.php
@@ -39,7 +39,7 @@ $productos = ControladorProductos::ctrMostrarProductos($item, $valor, $orden);
 
     <?php
 
-    for($i = 0; $i < 10; $i++){
+    for($i = 0; $i < 8; $i++){
 
       echo '<li class="item">
 
